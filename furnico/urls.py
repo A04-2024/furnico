e27ece2c1f720e11120c7cb212b5034d099a3668
vito1012buss <thebestvitodiaries@gmail.com>
2024-10-22 06:24:14 +0800

    konfigurasi url articles di settings.py furnico

--- furnico/urls.py
+++ furnico/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('show_products.urls')),
+    path('articles/', include('article.urls'))
 ]
