bd2392b8800bb12e46a975d8d88bc1dc743fed42
theoalll <theo.ananda@ui.ac.id>
2024-10-22 21:17:35 +0700

    add show xml and json

--- show_products/urls.py
+++ show_products/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('dev', show_main, name='show_main'),
     path('create_product', create_product_entry, name='create_product_entry'),
     path('create_category', create_category, name='create_category'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- show_products/views.py
+++ show_products/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from show_products.forms import *
 from show_products.models import *
 
+# returns data in xml and json
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_products(request):
@@ -45,3 +48,19 @@ def create_category(request):
 
     context = {'form': form}
     return render(request, "create_category.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
