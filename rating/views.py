7e322256275fb18b841bff2bc7c1d91050163e73
patriciatyas <patriciatyas11@gmail.com>
2024-10-23 22:47:46 +0700

    add views.py for rating

--- rating/views.py
+++ rating/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ProductRating, Product
 
-def show_rating(request):
+def show_rating(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    ratings = ProductRating.objects.filter(product=product).order_by('-created_at')
+
     context = {
-        'rating': 5
+        'product': product,
+        'ratings': ratings
     }
 
     return render(request, "rating.html", context)
