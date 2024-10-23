c12dc107c0a6b7ad83fc768a3a4af9983d743f58
patriciatyas <patriciatyas11@gmail.com>
2024-10-24 01:04:15 +0700

    make add rating page

--- rating/urls.py
+++ rating/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import show_rating 
+from rating.views import show_rating, edit_rating, delete_rating, add_rating_ajax, show_xml, show_json, show_xml_by_id, show_json_by_id
 
-app_name = 'rating' 
+app_name = 'rating'
 
 urlpatterns = [
-    path('products/<uuid:product_id>/ratings/', show_rating, name='show_rating'),
+    path('', show_rating, name='show_rating'),
+    path('edit-rating/<uuid:id>', edit_rating, name='edit_rating'),
+    path('delete/<uuid:id>', delete_rating, name='delete_rating'),
+    path('create-rating-ajax', add_rating_ajax, name='add_rating_ajax'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
--- rating/views.py
+++ rating/views.py
@@ -1,14 +1,72 @@
 from django.shortcuts import render, get_object_or_404
-from .models import ProductRating, Product
+from show_products.models import Product
+from rating.forms import ProductRatingForm
+from rating.models import ProductRating
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core import serializers
 
-def show_rating(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
 
-    ratings = ProductRating.objects.filter(product=product).order_by('-created_at')
+def show_rating(request):
+    ratings = ProductRating.objects.all() 
 
     context = {
-        'product': product,
-        'ratings': ratings
+        'title': "Recent Review",
+        'ratings': ratings  # Pass all ratings to the template
     }
 
-    return render(request, "rating.html", context)
+    return render(request, 'rating.html', context)
+
+
+@csrf_exempt
+@require_POST
+def add_rating_ajax(request):
+    form = ProductRatingForm(request.POST)
+    
+    if form.is_valid():
+        rating = form.save(commit=False) 
+        rating.user = request.user  
+        rating.save()
+        return HttpResponse("CREATED", status=201)
+    else:
+        # Send back an error message with status 400
+        error_message = ', '.join([f"{field}: {error[0]}" for field, error in form.errors.items()])
+        return HttpResponse(f"Error: {error_message}", status=400)
+    
+def edit_rating(request, id):
+    # Get the product entry
+    rating = ProductRating.objects.get(pk=id)
+
+    # Set product entry sebagai instance dari form
+    form = ProductRatingForm(request.POST or None, instance=rating)
+
+    if form.is_valid() and request.method == "POST":
+        form.save() # simpan form dan kembali ke halaman awal
+        return HttpResponseRedirect(reverse('rating:show_rating'))
+    
+    context = {'form': form}
+    return render(request, "edit_rating.html", context)
+
+def delete_rating(request, id):
+    rating = ProductRating.objects.get(pk=id) # Get product berdasarkan id
+    rating.delete() # hapus product
+    return HttpResponseRedirect(reverse('rating:show_rating'))
+
+def show_xml(request):
+    data = ProductRating.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("xml", data), content_type='application/xml')
+
+def show_json(request):
+    data = ProductRating.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json", data), content_type='application/json')
+
+def show_xml_by_id(request, id):
+    data = ProductRating.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductRating.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
