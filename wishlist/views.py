e21e385a895a9a5f08cdea6fb59752b41ce1b16c
sarahsaphira <sarahsaphira80@gmail.com>
2024-10-24 18:57:39 +0700

    wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from .models import WishlistItem, Collection
 from show_products.models import Product
 from .forms import CollectionForm
 from django.contrib.auth.models import User
+import json
+from django.views.decorators.csrf import csrf_exempt
+
 
 def mock_user():
     user, created = User.objects.get_or_create(username='mockuser', id=1)
@@ -19,13 +23,11 @@ def add_to_wishlist(request, product_id):
             collection = get_object_or_404(Collection, id=collection_id, user=user)
         else:
             collection, created = Collection.objects.get_or_create(user=user, name="Semua Wishlist")
-        
         WishlistItem.objects.create(product=product, collection=collection)
-        return redirect('wishlist:wishlist') 
-    
+        return JsonResponse({'success': True})
+
     collections = Collection.objects.filter(user=user)
     return render(request, 'add_to_wishlist.html', {'product': product, 'collections': collections})
-
 
 # @login_required
 def wishlist_view(request):
@@ -33,27 +35,57 @@ def wishlist_view(request):
     collections = Collection.objects.filter(user=user)
     return render(request, 'wishlist.html', {'collections': collections})
 
+def wishlist_json_view(request):
+    user = mock_user()
+    collections = Collection.objects.filter(user=user).prefetch_related('wishlistitem_set__product')
+
+    wishlist_data = []
+    for collection in collections:
+        collection_data = {
+            'collection_id': collection.id,
+            'collection_name': collection.name,
+            'items': [
+                {
+                    'product_id': item.product.id,
+                    'product_name': item.product.product_name
+                }
+                for item in collection.wishlistitem_set.all()
+            ]
+        }
+        wishlist_data.append(collection_data)
+
+    return JsonResponse({'collections': wishlist_data})
 
 # @login_required
+@csrf_exempt
 def create_collection(request):
     user = mock_user()
-    if request.method == 'POST':
-        form = CollectionForm(request.POST)
-        if form.is_valid():
-            collection = form.save(commit=False)
-            collection.user = user
-            collection.save()
-            return redirect('wishlist:wishlist')  
-    else:
-        form = CollectionForm()
-    return render(request, 'create_collection.html', {'form': form})
 
-# @login_required
-def remove_wishlist(request, wishlist_item_id):
-    user = mock_user()
-    wishlist_item = get_object_or_404(WishlistItem, id=wishlist_item_id, collection__user=user)
-    wishlist_item.delete()
-    return redirect('wishlist:wishlist')
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            collection_name = data.get('collection_name')
+            form = CollectionForm({'name': collection_name})
+
+            if form.is_valid():
+                collection = form.save(commit=False)
+                collection.user = user
+                collection.save()
+
+                return JsonResponse({
+                    'success': True,
+                    'collection': {
+                        'id': collection.id,
+                        'name': collection.name
+                    }
+                })
+
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'message': 'Invalid JSON data'})
+
+    return JsonResponse({'success': False, 'message': 'Invalid request method'})
 
 # @login_required
 def update_collection_name(request, collection_id):
@@ -61,14 +93,26 @@ def update_collection_name(request, collection_id):
     collection = get_object_or_404(Collection, id=collection_id, user=user)
 
     if request.method == 'POST':
-        form = CollectionForm(request.POST, instance=collection)
-        if form.is_valid():
-            form.save()
-            return redirect('wishlist:wishlist')
-    else:
-        form = CollectionForm(instance=collection)
-    
-    return render(request, 'update_collection.html', {'form': form, 'collection': collection})
+        try:
+            data = json.loads(request.body)
+            collection_name = data.get('collection_name')
+            form = CollectionForm({'name': collection_name}, instance=collection)
+
+            if form.is_valid():
+                form.save()
+                return JsonResponse({
+                    'success': True,
+                    'collection': {
+                        'id': collection.id,
+                        'name': collection.name
+                    }
+                })
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'message': 'Invalid JSON data'})
+
+    return JsonResponse({'success': False, 'message': 'Invalid request method'})
 
 # @login_required
 def delete_collection(request, collection_id):
@@ -76,13 +120,19 @@ def delete_collection(request, collection_id):
     collection = get_object_or_404(Collection, id=collection_id, user=user)
 
     if collection.name == "Semua Wishlist":
-        return redirect('wishlist:wishlist')
+        return JsonResponse({'success': False, 'message': 'Cannot delete default collection'})
 
     default_collection, created = Collection.objects.get_or_create(user=user, name="Semua Wishlist")
 
     collection_items = collection.wishlistitem_set.all()
     collection_items.update(collection=default_collection)
-
     collection.delete()
 
-    return redirect('wishlist:wishlist')
+    return JsonResponse({'success': True})
+
+# @login_required
+def remove_wishlist(request, wishlist_item_id):
+    user = mock_user()
+    wishlist_item = get_object_or_404(WishlistItem, id=wishlist_item_id, collection__user=user)
+    wishlist_item.delete()
+    return JsonResponse({'success': True})
