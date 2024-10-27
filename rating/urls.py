2553dd38a7b83c1a5297f3ea20dc49bdd099de83
patriciatyas <patriciatyas11@gmail.com>
2024-10-27 22:42:46 +0700

    finish rating app

--- rating/urls.py
+++ rating/urls.py
@@ -6,8 +6,8 @@ app_name = 'ratings'
 
 urlpatterns = [
     path('product/<uuid:id>/add/', views.add_rating, name='add_rating'),
-    path('<uuid:rating_id>/edit', views.edit_rating, name='edit_rating'),
-    path('<uuid:rating_id>/delete', views.delete_rating, name='delete_rating'),
+    path('product/<uuid:rating_id>/edit', views.edit_rating, name='edit_rating'),
+    path('product/<uuid:rating_id>/delete', views.delete_rating, name='delete_rating'),
     path('product/<uuid:id>/add-rating-ajax/', views.add_rating_ajax, name='add_rating_ajax'),
     path('json/', views.show_json, name='show_json'),
 ]
